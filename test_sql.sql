22efff48b07394ce75594bc25d940d2ebb32902e
mrafalcon <asokolov.photo@gmail.com>
2023-03-10 13:58:19 +0300

    test 1

--- test_sql.sql
+++ test_sql.sql
@@ -1,1 +1,6 @@
-select * from EMP_DETAILS_VIEW;
+--EXPLAIN PLAN FOR
+--select distinct DEPARTMENT_ID, sum(SALARY) from EMP_DETAILS_VIEW
+--having sum(salary) >= 5000
+--group by DEPARTMENT_ID
+select trunc(salary, -3), round(salary, -3) from EMP_DETAILS_VIEW
+where JOB_ID like 'ST_%'
