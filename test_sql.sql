bc90dac51c452ccee6591b0715d6b39419f7b51e
mrafalcon <asokolov.photo@gmail.com>
2023-03-10 21:40:12 +0300

    test learn

--- test_sql.sql
+++ test_sql.sql
@@ -1,6 +1,7 @@
 --EXPLAIN PLAN FOR
---select distinct DEPARTMENT_ID, sum(SALARY) from EMP_DETAILS_VIEW
---having sum(salary) >= 5000
---group by DEPARTMENT_ID
-select trunc(salary, -3), round(salary, -3) from EMP_DETAILS_VIEW
+select distinct JOB_ID, sum(SALARY) from EMP_DETAILS_VIEW
 where JOB_ID like 'ST_%'
+group by JOB_ID
+having sum(salary) >= 5000
+--select job_id, salary, trunc(salary, -3), round(salary, -3) from EMP_DETAILS_VIEW
+--where JOB_ID like 'ST_%'
